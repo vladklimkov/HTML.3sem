248e755ada686123726574e67effe64e54ad8ab1
vladklimkov <128241526+vladklimkov@users.noreply.github.com>
2024-02-12 23:29:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+
+function myFunction() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
